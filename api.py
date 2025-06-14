92f51a0b3046e7ab0237fdc4148372f5ff68a73d
Gamal Elbatawy <125583756+gamalgithue@users.noreply.github.com>
2025-06-14 19:25:09 +0300

    Update api.py

--- api.py
+++ api.py
@@ -9,6 +9,3 @@ def get_recommendations(request: RecommendationRequest):
     recommended_trips = recommend_trips(request)
     return {"recommended_trips": recommended_trips}
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
